1b22a9ee6c70d81a3a89bdb9fc17888f9a3eac39
davifranca <davicavalcantefranca@gmail.com>
2025-11-26 11:39:10 -0300

    feat: adicionado model

--- src/main/crud/produto/controller/ProdutoController.java
+++ src/main/crud/produto/controller/ProdutoController.java
@@ -1,28 +1,31 @@
 package main.crud.produto.controller;
 
+import main.crud.produto.model.Produto;
+
 import java.util.ArrayList;
 import java.util.List;
 
 // TODO: Implementar com a classe model quando estiver pronto
 public class ProdutoController {
-    private List<ProdutoController> produtos;
+    private List<Produto> produtos;
 
     public ProdutoController() {
+
         produtos = new ArrayList<>();
     }
 
-    public void adicionar(ProdutoController produto) {
+    public void adicionar(Produto produto) {
         if (produto != null) {
             produtos.add(produto);
         }
     }
 
-    public List<ProdutoController> listar() {
+    public List<Produto> listar() {
         return new ArrayList<>(produtos);
     }
 
-    public ProdutoController buscarPorId(int id) {
-        for (ProdutoController p : produtos) {
+    public Produto buscarPorId(int id) {
+        for (Produto p : produtos) {
             if (p.getId() == id) {
                 return p;
             }
@@ -30,7 +33,7 @@ public class ProdutoController {
         return null;
     }
 
-    public boolean atualizar(int id, ProdutoController produtoAtualizado) {
+    public boolean atualizar(int id, Produto produtoAtualizado) {
         for (int i=0; i<produtos.size();i++){
             if (produtos.get(i).getId() == id){
                 produtoAtualizado.setId(id);
@@ -46,10 +49,12 @@ public class ProdutoController {
     }
 
     public int totalProdutos(){
+
         return produtos.size();
     }
 
     public boolean existeProduto(int id){
         return buscarPorId(id) != null;
+
     }
 }
--- src/main/crud/produto/model/Produto.java
+++ src/main/crud/produto/model/Produto.java
@@ -1,4 +1,10 @@
 package main.crud.produto.model;
 
 public class Produto {
+    private int id;
+    private String nome;
+    private double preco;
+    private int quantidade;
+
+    private final int proximoId = 1;
 }

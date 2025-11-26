ee31711ea0bb1abd9817a8eee8ee7b1004f50d1f
davifranca <davicavalcantefranca@gmail.com>
2025-11-26 19:10:13 -0300

    fix: arrumado teste do modelo

--- src/main/teste/ProdutoTeste.java
+++ src/main/teste/ProdutoTeste.java
@@ -6,7 +6,7 @@ import main.crud.produto.model.Produto;
 public class ProdutoTeste {
     public static void main(String[] args){
         ProdutoController controller = new ProdutoController();
-        Produto produto1 = new Produto("Hamburguer", "Hamburguer com cheddar", 13.50,50);
+        Produto produto1 = new Produto("ZBurger", "Hamburguer com cheddar", 13.50,50);
         Produto produto2 = new Produto("Filé Mignom", "Filé Mignom com Batata Frita", 45.50,50);
         System.out.println(produto1.toString());
         System.out.println(produto2.toString());
